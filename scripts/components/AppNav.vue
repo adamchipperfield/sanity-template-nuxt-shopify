6dd3ade0b501441b7fb5fb772b7c296995c39bf0
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2020-07-27 23:12:37 +0100

    BUILD: Updated the App Nav to use a static array

--- scripts/components/AppNav.vue
+++ scripts/components/AppNav.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="app-nav">
-    <n-link class="app-nav__item" to="/collections/frontpage">
-      Mens
-    </n-link>
-
-    <n-link class="app-nav__item" to="/collections/new-in">
-      New In
-    </n-link>
-
-    <n-link class="app-nav__item" to="/blogs/news">
-      Blog
-    </n-link>
+    <ul class="app-nav__menu">
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        class="app-nav__item"
+      >
+        <n-link class="app-nav__link" :to="item.url">
+          {{ item.label }}
+        </n-link>
+      </li>
+    </ul>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          label: 'Collections',
+          url: '/'
+        },
+        {
+          label: 'Blog',
+          url: '/blogs/news'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 .app-nav {
+  &__menu {
+    @include list-reset;
+    display: flex;
+  }
+
   &__item {
+    position: relative;
+  }
+
+  &__link {
     margin-right: $SPACING_XL;
   }
 }
