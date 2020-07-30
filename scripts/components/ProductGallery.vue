9d60b10f7465e05562ddd5c3d921ee1051a9ab21
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2020-07-30 18:55:55 +0100

    BUILD: Added pagination bullets to the Product Gallery

--- scripts/components/ProductGallery.vue
+++ scripts/components/ProductGallery.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="product-gallery">
-    <div ref="carousel" class="swiper-container">
+    <div
+      ref="carousel"
+      class="product-gallery__carousel swiper-container"
+    >
       <div class="swiper-wrapper">
         <div
           v-for="(image, index) in images"
@@ -15,12 +18,17 @@
           />
         </div>
       </div>
+
+      <div
+        ref="pagination"
+        class="product-gallery__pagination"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper } from 'swiper'
+import { Swiper, Pagination } from 'swiper'
 
 import ResponsiveImage from '~/components/ResponsiveImage'
 
@@ -38,10 +46,19 @@ export default {
   },
 
   mounted() {
+    this.initializeSwiper()
     this.constructCarousel()
   },
 
   methods: {
+    /**
+     * Initializes the Swiper module.
+     * - Adds the components to be used.
+     */
+    initializeSwiper() {
+      Swiper.use(Pagination)
+    },
+
     /**
      * Constructs the carousel instance.
      */
@@ -49,7 +66,11 @@ export default {
       this.carousel = new Swiper(this.$refs.carousel, {
         slidesPerView: 1,
         slidesPerGroup: 1,
-        loop: true
+        loop: true,
+        pagination: {
+          el: this.$refs.pagination,
+          clickable: true
+        }
       })
     }
   }
@@ -58,4 +79,40 @@ export default {
 
 <style lang="scss">
 @import '~swiper/swiper';
+
+.product-gallery {
+  &__carousel {
+    position: relative;
+  }
+
+  &__pagination {
+    bottom: $SPACING_L;
+    display: flex;
+    left: $SPACING_L;
+    position: absolute;
+    z-index: 1;
+  }
+
+  .swiper-pagination-bullet {
+    background-color: lighten($COLOR_TEXT_LIGHT, 50%);
+    border-radius: 50%;
+    cursor: pointer;
+    display: block;
+    height: 8px;
+    margin-right: $SPACING_XS;
+    transition: background-color 0.2s ease-out;
+    width: 8px;
+  }
+
+  .swiper-pagination-bullet-active {
+    background-color: $COLOR_TEXT_LIGHT;
+  }
+
+  @include mq($from: large) {
+    &__pagination {
+      bottom: $SPACING_2XL;
+      left: $SPACING_2XL;
+    }
+  }
+}
 </style>
