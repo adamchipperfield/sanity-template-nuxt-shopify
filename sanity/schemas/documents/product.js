9c2832946e9f76865b29de1ed45ca207c8f9190a
Adam Chipperfield <adam.chipperfield@wemakewebsites.com>
2020-08-11 19:56:05 +0100

    BUILD: Settled on a final product basic document structure

--- sanity/schemas/documents/product.js
+++ sanity/schemas/documents/product.js
@@ -24,11 +24,38 @@ export default {
       inputComponent: ShopifyProduct
     },
     {
+      title: 'Images',
+      name: 'images',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          fields: [
+            {
+              title: 'Title',
+              name: 'title',
+              type: 'string'
+            }
+          ]
+        }
+      ]
+    },
+    {
       title: 'Description',
       name: 'description',
       type: 'array',
       of: [
-        { type: 'block' }
+        { type: 'block' },
+        {
+          type: 'image',
+          fields: [
+            {
+              title: 'Title',
+              name: 'title',
+              type: 'string'
+            }
+          ]
+        }
       ]
     }
   ]
